5ad13b107264e0bcbfa7429c2dcf30cc7780060c
KerryLin11 <84896346+KerryLin11@users.noreply.github.com>
2024-03-20 00:38:18 +1100

    dash and wasd infrastructure, scripts folder, input system

---
+++ THE GAME/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Movement
+
+    //WASD
+    [SerializeField] private float moveSpeed = 4f;
+    private Vector2 moveDirection;
+
+    //Dash
+    private Rigidbody2D rb;
+    [SerializeField] private float dashSpeed = 12f;
+    [SerializeField] private float dashDuration = 0.2f;
+    [SerializeField] private float dashCooldown = 1f;
+    
+    // Unity Input System
+    private PlayerActions playerActions;
+    private InputAction WASDInput;
+    private InputAction dashInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        playerActions = new PlayerActions();
+        WASDInput = playerActions.Movement.Walk;
+        dashInput = playerActions.Movement.Dash;
+
+        // Input Bindings
+
+        //whenever a WASDInput is performed, update the moveDirection
+        WASDInput.performed += ctx => moveDirection = ctx.ReadValue<Vector2>(); 
+        WASDInput.canceled += ctx => moveDirection = Vector2.zero;
+        //call Dash() on 'Space'
+        dashInput.performed += ctx => Dash();
+    }
+
+    void OnEnable()
+    {
+        playerActions.Enable();
+    }
+
+    void OnDisable()
+    {
+        playerActions.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.Translate(moveDirection * moveSpeed);
+    }
+
+    void Dash()
+    {
+        Debug.Log("dash");
+
+        //TODO: Complete Dash Implementation
+    }
+}
