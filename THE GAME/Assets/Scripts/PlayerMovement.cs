a6f707693550aaaf461f0cafad90c09f1233df1f
ratquaza <baitoyawanis@gmail.com>
2024-03-20 17:13:10 +1100

    player controller and dash

--- THE GAME/Assets/Scripts/PlayerMovement.cs
+++ THE GAME/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 4f;
+    [SerializeField] private float dampening = 0.85f;
     [SerializeField] private float dashSpeed = 12f;
     [SerializeField] private float dashDuration = 0.2f;
     [SerializeField] private float dashCooldown = 1f;
@@ -13,20 +14,20 @@ public class PlayerMovement : MonoBehaviour
     private PlayerActions playerActions;
     private InputAction WASDInput;
     private InputAction dashInput;
-    private Rigidbody2D body;
+    private Rigidbody2D rb2d;
+
+    private float currentDashDuration = 0f;
+    private float currentDashCooldown = 0f;
+    private Vector2 dashDirection = Vector2.zero;
 
     void Awake()
     {
-        body = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
         playerActions = new PlayerActions();
 
         WASDInput = playerActions.Movement.Walk;
         dashInput = playerActions.Movement.Dash;
-        playerActions.Movement.Enable();
 
-        WASDInput.performed += ctx => body.velocity += ctx.ReadValue<Vector2>() * moveSpeed;
-    
-        //call Dash() on 'Space'
         dashInput.performed += ctx => Dash();
     }
 
@@ -40,9 +41,25 @@ public class PlayerMovement : MonoBehaviour
         playerActions.Disable();
     }
 
+    void Update()
+    {
+        if (currentDashDuration <= 0f) {
+            rb2d.velocity += WASDInput.ReadValue<Vector2>() * moveSpeed;
+            rb2d.velocity *= dampening;
+            if (currentDashCooldown > 0) currentDashCooldown -= Time.deltaTime;
+        } else {
+            rb2d.velocity = dashDirection * dashSpeed;
+            currentDashDuration -= Time.deltaTime;
+            if (currentDashDuration <= 0) {
+                currentDashCooldown = dashCooldown;
+            }
+        }
+    }
+
     void Dash()
     {
-        Debug.Log("dash");
-        //TODO: Complete Dash Implementation
+        if (currentDashCooldown > 0) return;
+        dashDirection = WASDInput.ReadValue<Vector2>();
+        currentDashDuration = dashDuration;
     }
 }
