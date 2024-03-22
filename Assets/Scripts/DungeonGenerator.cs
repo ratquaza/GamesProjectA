28d67d782ac167087e923b36e41fcac0952f83af
ratquaza <baitoyawanis@gmail.com>
2024-03-23 00:43:25 +1100

    rooms now connect to eachother - need to do overlap detection

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,42 +1,58 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    [SerializeField] private GameObject testRoom;
+    [SerializeField] private GameObject[] rooms;
+    [SerializeField] private int count = 5;
+    private readonly string[] exitNames = new string[4] { "NorthExit", "EastExit", "SouthExit", "WestExit" };
+    private Grid grid;
 
     // Start is called before the first frame update
     void Start()
     {
-        Grid grid = GetComponent<Grid>();
-        int count = 2;
+        grid = GetComponent<Grid>();
 
-        GameObject lastRoom = null;
+        GameObject chosenRoom = Instantiate(RandomRoom(), grid.transform);
+        Tilemap chosenTm = chosenRoom.GetComponent<Tilemap>();
+        chosenTm.CompressBounds();
+        Bounds chosenBounds = chosenTm.localBounds;
 
-        while (count > 0)
+        chosenRoom.transform.localPosition = Vector3.zero - chosenBounds.center + chosenBounds.size / 2;
+        chosenRoom.name = "SpawnRoom";
+        foreach (GameObject secondRoom in FillExits(chosenRoom))
         {
-            GameObject chosenRoom = Instantiate(testRoom, grid.transform);
-            Tilemap chosenTm = chosenRoom.GetComponent<Tilemap>();
-            chosenTm.CompressBounds();
-            Bounds chosenBounds = chosenTm.localBounds;
+            FillExits(secondRoom);
+        }
+    }
 
-            if (lastRoom != null)
-            {
-                GameObject lastExit = lastRoom.transform.Find("WestExit").gameObject;
-                GameObject chosenExit = chosenRoom.transform.Find("EastExit").gameObject;
-                AlignByAnchors(chosenRoom, chosenExit, lastExit);
-            } else 
-            {
-                chosenRoom.transform.localPosition = Vector3.zero - chosenBounds.center;
-            }
+    GameObject[] FillExits(GameObject room, int generateChance = 1)
+    {
+        List<GameObject> generatedRooms = new List<GameObject>();
+        generateChance = Math.Min(generateChance, 1);
 
-            lastRoom = chosenRoom;
-            count--;
+        for (int i = 0; i < room.transform.childCount; i++)
+        {
+            Transform child = room.transform.GetChild(i);
+            if (!exitNames.Contains(child.name)) continue;
+            string opposingExit = exitNames[(Array.IndexOf(exitNames, child.name) + 2) % 4];
+
+            GameObject[] connectingRooms = rooms.Where((go) => go.transform.Find(opposingExit) != null).ToArray();
+            if (connectingRooms.Length == 0) continue;
+            GameObject chosenRoom = Instantiate(connectingRooms[UnityEngine.Random.Range(0, connectingRooms.Length)], grid.transform);
+            chosenRoom.GetComponent<Tilemap>().CompressBounds();
+
+            generatedRooms.Add(chosenRoom);
+            GameObject chosenRoomExit = chosenRoom.transform.Find(opposingExit).gameObject; 
+            AlignByAnchors(chosenRoom, chosenRoomExit, child.gameObject);
+            DestroyImmediate(child.gameObject);
+            DestroyImmediate(chosenRoomExit);
         }
 
+        return generatedRooms.ToArray();
     }
 
     void AlignByAnchors(GameObject target, GameObject targetAnchor, GameObject otherAnchor)
@@ -47,9 +63,14 @@ public class DungeonGenerator : MonoBehaviour
         target.transform.localPosition = otherAnchor.transform.position - targetBounds.center + targetAnchorOffset;
     }
 
+    GameObject RandomRoom()
+    {
+        return rooms[UnityEngine.Random.Range(0, rooms.Length)];
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
