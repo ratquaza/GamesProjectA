61e5dc85d781c8b918a16a682e9fb23a33213ffb
Luca Battaglia <gianluca.battaglia1@students.mq.edu.au>
2024-04-03 14:39:46 +1100

    followMouse Update
    + Fixes followMouse syntax error

--- Assets/Scripts/followMouse.cs
+++ Assets/Scripts/followMouse.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowMouse : MonoBehaviour
+public class followMouse : MonoBehaviour
 {   
     void Update()
     {
