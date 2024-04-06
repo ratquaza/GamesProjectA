da568cb1bcb28367a38b359008bed8c933ee7f68
ratquaza <baitoyawanis@gmail.com>
2024-04-06 14:50:09 +1100

    ItemDatabase script

---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public static ItemDatabase Instance { get; private set; }
+
+    [SerializeField] private List<Item> toLoad = new List<Item>();
+    public Dictionary<string, Item> Items { get; private set; }
+
+    void Start()
+    {
+        if (Instance != null) Destroy(Instance.gameObject);
+        Instance = this;
+        Items = toLoad.ToDictionary((item) => item.name);
+        Object.DontDestroyOnLoad(this.gameObject);
+    }
+}
