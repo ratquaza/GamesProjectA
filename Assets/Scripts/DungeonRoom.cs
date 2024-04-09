d728a347b01de05fe5a52cadc23855fe8d0e8a10
ratquaza <baitoyawanis@gmail.com>
2024-04-09 23:20:57 +1000

    dungeon rooms now enable/disale enemies inside of them

--- Assets/Scripts/DungeonRoom.cs
+++ Assets/Scripts/DungeonRoom.cs
@@ -7,12 +7,21 @@ public class DungeonRoom : MonoBehaviour
     [SerializeField] private BoxCollider2D boundsCollider;
     [SerializeField] private Vector2 boundsOffset;
     [SerializeField] private CameraFollow cameraScript;
+    
+    void Start()
+    {
+
+    }
 
     void OnTriggerStay2D(Collider2D data)
     {
         PlayerLiving player = data.GetComponent<PlayerLiving>();
         if (player == null) return;
         cameraScript.SetBounds(transform.TransformPoint(boundsCollider.offset), boundsCollider.size + boundsOffset);
+    }
+
+    void OnTriggerExit2D(Collider2D data)
+    {
     }
 
     [ExecuteInEditMode]
