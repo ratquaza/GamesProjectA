dfca681b5f83c6679ad1aff2a3afb73b9dc7b430
ratquaza <baitoyawanis@gmail.com>
2024-04-09 21:54:09 +1000

    Mana Tome weapon

---
+++ Assets/Scripts/Bullet/ManaBubbleProjectile.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaBubbleProjectile : MonoBehaviour
+{
+    [SerializeField] protected CircleCollider2D aoe;
+    [SerializeField] protected float readyTime = 1f;
+    [SerializeField] protected float popTime = 0.2f;
+
+    protected float currentReadyTime = 0f;
+    protected float currentPopTime = 0f;
+    protected bool isReady = false;
+    protected bool isPopping = false;
+    protected Vector3 initialScale;
+
+    void Start()
+    {
+        initialScale = transform.localScale;
+        transform.localScale *= .1f;
+        aoe.enabled = false;
+        currentReadyTime = readyTime;
+        currentPopTime = popTime;
+    }
+
+    void Update()
+    {
+        if (!isReady && currentReadyTime >= 0)
+        {
+            currentReadyTime -= Time.deltaTime;
+            transform.localScale = Vector3.Lerp(initialScale, transform.localScale, currentReadyTime/readyTime);
+            if (currentReadyTime <= 0) OnReady();
+        }
+        if (isReady && isPopping && currentPopTime >= 0)
+        {
+            currentPopTime -= Time.deltaTime;
+            if (currentPopTime <= 0) OnPop();
+        }
+    }
+
+    protected void OnReady()
+    {
+        isReady = true;
+        aoe.enabled = true;
+    }
+
+    protected void OnPop()
+    {
+        List<Collider2D> hits = new List<Collider2D>();
+        ContactFilter2D filter = new ContactFilter2D();
+        aoe.OverlapCollider(filter, hits);
+        foreach (var hit in hits)
+        {
+            Enemy enemy = hit.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                Rigidbody2D rb2d = hit.GetComponent<Rigidbody2D>();
+                enemy.Damage(5);
+                rb2d.velocity += (Vector2) (rb2d.transform.position - transform.position).normalized * 80f;
+            }
+            ManaBubbleProjectile bubble = hit.GetComponent<ManaBubbleProjectile>();
+            if (bubble != null) bubble.SetToPop();
+        }
+        Destroy(gameObject);
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (isReady && collision.collider.GetComponent<ManaBubbleProjectile>() == null) isPopping = true;
+    }
+
+    public void SetToPop()
+    {
+        isReady = true;
+        isPopping = true;
+    }
+}
---
+++ Assets/Scripts/Weapon/ManaTome.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ManaTome : SimpleWeapon
+{
+    [SerializeField] protected GameObject manaBubble;
+    private Vector2 gizmoPos;
+    public static readonly float PRIMARY_MAX_DISTANCE = 5f;
+
+    public ManaTome() : base(.4f, 2f)
+    {}
+
+    protected override void OnPrimary(InputAction.CallbackContext ctx)
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = player.transform.InverseTransformPoint(mousePos);
+        mousePos = Vector2.ClampMagnitude(mousePos, PRIMARY_MAX_DISTANCE);
+        mousePos = player.transform.TransformPoint(mousePos);
+        gizmoPos = mousePos;
+        DamageInSquare(mousePos, Vector2.one, 1, (enemy) => ((Vector2) enemy.transform.position - mousePos).normalized * 20f);
+    }
+
+    protected override void OnSecondary(InputAction.CallbackContext ctx)
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject bubble = Instantiate(manaBubble);
+            Vector2 offset = new Vector2(Random.Range(-1.5f, 1.5f), Random.Range(-1.5f, 1.5f));
+            bubble.transform.position = mousePos + offset;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(gizmoPos, Vector2.one);
+    }
+}
