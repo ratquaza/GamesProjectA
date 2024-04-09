fddd5bb596555ec5ff92054b188d5edff181de54
ratquaza <baitoyawanis@gmail.com>
2024-04-09 15:00:57 +1000

    dungeon room now has offset vector and visualisations while editing

--- Assets/Scripts/DungeonRoom.cs
+++ Assets/Scripts/DungeonRoom.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class DungeonRoom : MonoBehaviour
 {
     [SerializeField] private BoxCollider2D boundsCollider;
+    [SerializeField] private Vector2 boundsOffset;
     [SerializeField] private CameraFollow cameraScript;
 
-    void OnTriggerEnter2D(Collider2D data)
+    void OnTriggerStay2D(Collider2D data)
     {
         PlayerLiving player = data.GetComponent<PlayerLiving>();
         if (player == null) return;
-        cameraScript.SetBounds(transform.TransformPoint(boundsCollider.offset), boundsCollider.size);
+        cameraScript.SetBounds(transform.TransformPoint(boundsCollider.offset), boundsCollider.size + boundsOffset);
+    }
+
+    [ExecuteInEditMode]
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(transform.TransformPoint(boundsCollider.offset), boundsCollider.size);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.TransformPoint(boundsCollider.offset), boundsCollider.size + boundsOffset);
     }
 }
