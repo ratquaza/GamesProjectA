d99ccf7da583befde0458b09008d6ec0cebec6e9
ratquaza <baitoyawanis@gmail.com>
2024-04-10 14:14:39 +1000

    arc weapon

--- Assets/Scripts/Bullet/NewArc.cs
+++ Assets/Scripts/Bullet/NewArc.cs
@@ -7,6 +7,8 @@ public class NewArc : MonoBehaviour
     [SerializeField] private GameObject projectile;
     [SerializeField] private PlayerMovement player;
     [SerializeField] private float shootTime = 2f;
+    [SerializeField] private float degree = 10f;
+    [SerializeField] private int shootCount = 7;
 
     private float curShootTime = 0f;
 
@@ -26,10 +28,10 @@ public class NewArc : MonoBehaviour
         Vector2 toPlayer = ((Vector2) (player.transform.position - transform.position)).normalized;
         float angle = Mathf.Atan2(toPlayer.y, toPlayer.x) * Mathf.Rad2Deg;
 
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < shootCount; i++)
         {
             GameObject bullet = Instantiate(projectile);
-            bullet.transform.rotation = Quaternion.Euler(Vector3.forward * (angle - 45 + (15 * i)));
+            bullet.transform.rotation = Quaternion.Euler(Vector3.forward * (angle - (degree * (shootCount/2)) + (degree * i)));
             bullet.transform.position = transform.position;
             Projectile proj = bullet.GetComponent<Projectile>();
             proj.projectileSpeed = 5f;
