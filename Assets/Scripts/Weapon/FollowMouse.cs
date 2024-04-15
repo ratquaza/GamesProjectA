a665a6a927e0868775bfcfe15c2f3ef5b6ea3c8e
ratquaza <baitoyawanis@gmail.com>
2024-04-15 20:11:19 +1000

    followmosue now lerps

--- Assets/Scripts/Weapon/FollowMouse.cs
+++ Assets/Scripts/Weapon/FollowMouse.cs
@@ -15,7 +15,7 @@ public class FollowMouse : MonoBehaviour
         Vector3 pos = Input.mousePosition - Camera.main.WorldToScreenPoint (transform.position);
 
 		float angle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+		transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(0, 0, angle)), 20f * Time.deltaTime);
 
         if (flipSprite) transform.localScale =  new Vector3(transform.localScale.x, angle >  90 || angle < -90 ? -initScale : initScale, transform.localScale.z);
     }
