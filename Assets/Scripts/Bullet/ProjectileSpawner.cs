2678a528237454368fc488444ab0091b67af5c01
ChiCuong <80371721+ReturnToNewbie@users.noreply.github.com>
2024-04-17 10:03:58 +1000

    update

--- Assets/Scripts/Bullet/ProjectileSpawner.cs
+++ Assets/Scripts/Bullet/ProjectileSpawner.cs
@@ -5,38 +5,60 @@ public class ProjectileSpawner : MonoBehaviour
     enum SpawnerType
     {
         Straight,
-        Spin
+        Spin,
+        NewArc
     }
 
     [Header("Projectile Attributes")]
     public GameObject projectilePrefab;
     [SerializeField] private float projectileLife = 10f;
     [SerializeField] private float projectileSpeed = 5f;
+    private float angle = 1 ;
 
     [Header("Spawner Attributes")]
     [SerializeField] private SpawnerType spawnerType;
     [SerializeField] private float firingRate = 1f;
+    [SerializeField] private float firingDistance = 10f;
+
+    [Header("NewArc")]
+    [SerializeField] private float degree = 10f;
+    [SerializeField] private int shootCount = 7;
+
+
+
 
     private float timer = 0f;
 
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
     void Update()
     {
-        timer += Time.deltaTime;
-
-        switch (spawnerType)
+     float distance = Vector2.Distance(transform.position, player.transform.position);
+            
+        if (distance < firingDistance)
         {
-            case SpawnerType.Straight:
-                UpdateStraightSpawner();
-                break;
-            case SpawnerType.Spin:
-                UpdateSpinSpawner();
-                break;
-        }
+            timer += Time.deltaTime;
+            
+            switch (spawnerType)
+            {
+                case SpawnerType.Straight:
+                    UpdateStraightSpawner();
+                    break;
+                case SpawnerType.Spin:
+                    UpdateSpinSpawner();
+                    break;
+            }
 
-        if (timer >= firingRate)
-        {
-            Fire();
-            timer = 0;
+            if (timer >= firingRate)
+            {
+                Fire();
+                timer = 0;
+            }
         }
     }
 
@@ -50,12 +72,27 @@ public class ProjectileSpawner : MonoBehaviour
         transform.Rotate(Vector3.forward, Time.deltaTime * 360f);
     }
 
+    public void UpdateNewArc()
+    {
+      
+    }
 
     private void Fire()
     {
+        if (spawnerType != SpawnerType.Spin)
+        {
+            Vector2 toPlayer = ((Vector2) (player.transform.position - transform.position)).normalized;
+            angle = Mathf.Atan2(toPlayer.y, toPlayer.x) * Mathf.Rad2Deg;  
+        }
         if (projectilePrefab)
         {
             GameObject spawnedProjectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+            
+            if (spawnerType != SpawnerType.Spin)
+            {
+                spawnedProjectile.transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+                spawnedProjectile.transform.position = transform.position;
+            }
             Projectile projectile = spawnedProjectile.GetComponent<Projectile>();
             if (projectile != null)
             {
